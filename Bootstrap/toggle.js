926007cb81622a6f688cc0abc8d9b0afa308b906
Linus Fernandes <linus.fernandes@gmail.com>
2024-03-11 15:16:03 +0530

    feat: add button examples

---
+++ Bootstrap/toggle.js
@@ -0,0 +1,7 @@
+function toggle() {
+  document.querySelectorAll(".btn").forEach((buttonElement) => {
+    const button = bootstrap.Button.getOrCreateInstance(buttonElement);
+    button.toggle();
+  });
+}
+setInterval(toggle, 1000);
